c85ec6deba8cc5b52ae303a0c45df37234889aae
obrien-ben <obrienbenjaminj@gmail.com>
2026-07-28 12:30:59 -0500

    Automated testing for filter_by_route GTFS helper

--- tests/testthat/test-gtfs_helpers.R
+++ tests/testthat/test-gtfs_helpers.R
@@ -92,3 +92,124 @@ test_that("get_gtfs_service_dates: date input range validation", {
     class = "error_gtfsdate_inputdata"
   )
 })
+
+test_that("filter_by_route: route validation", {
+
+  # One route
+  expect_error(
+    filter_by_route(gtfs = lacmta_gtfs,
+                    route_ids = "does not exist"),
+    class = "error_gtfsfilt_none"
+  )
+
+  # Multiple routes, neither exist
+  expect_error(
+    filter_by_route(gtfs = lacmta_gtfs,
+                    route_ids = c("does not exist", "also does not exist")),
+    class = "error_gtfsfilt_none"
+  )
+
+  # Multiple routes, one doesn't exist
+  expect_equal(
+    unique(filter_by_route(gtfs = lacmta_gtfs,
+                           route_ids = c("does not exist", "804"))$routes$route_id),
+    expected = "804"
+  )
+})
+
+test_that("filter_by_route: direction validation", {
+
+  # One dir
+  expect_error(
+    filter_by_route(gtfs = lacmta_gtfs,
+                    route_ids = "804",
+                    dir_id = 10),
+    class = "error_gtfsfilt_none"
+  )
+
+  # Multiple dirs
+  expect_error(
+    filter_by_route(gtfs = lacmta_gtfs,
+                    route_ids = "804",
+                    dir_id = c(10, 11)),
+    class = "error_gtfsfilt_none"
+  )
+
+  # Multiple dirs, one doesn't exist
+  expect_equal(
+    unique(filter_by_route(gtfs = lacmta_gtfs,
+                           route_ids = "804",
+                           c(0, 10))$trips$direction_id),
+    expected = 0
+  )
+})
+
+test_that("filter_by_route: route expectations", {
+
+  # Filter to one route
+  gtfs_filt_1 <- filter_by_route(gtfs = lacmta_gtfs,
+                                 route_ids = "804")
+  expect_equal(unique(gtfs_filt_1$routes$route_id),
+               expected = "804")
+  expect_equal(unique(gtfs_filt_1$trips$direction_id),
+               expected = c(0, 1))
+  expect_s3_class(gtfs_filt_1,
+                  class = "tidygtfs")
+
+  # Filter to multiple routes
+  gtfs_filt_2 <- filter_by_route(gtfs = lacmta_gtfs,
+                                 route_ids = c("801", "804"))
+  expect_equal(unique(gtfs_filt_2$routes$route_id),
+               expected = c("801", "804"))
+  expect_equal(unique(gtfs_filt_2$trips$direction_id),
+               expected = c(0, 1))
+  expect_s3_class(gtfs_filt_2,
+                  class = "tidygtfs")
+})
+
+test_that("filter_by_route: direction expectations", {
+
+  # Filter to one route, one dir
+  gtfs_filt_2 <- filter_by_route(gtfs = lacmta_gtfs,
+                                 route_ids = "804",
+                                 dir_id = 0)
+  expect_equal(unique(gtfs_filt_2$routes$route_id),
+                   expected = "804")
+  expect_equal(unique(gtfs_filt_2$trips$direction_id),
+                   expected = c(0))
+  expect_s3_class(gtfs_filt_2,
+                  class = "tidygtfs")
+
+  # Filter to one route, multiple dir
+  gtfs_filt_3 <- filter_by_route(gtfs = lacmta_gtfs,
+                                 route_ids = "804",
+                                 dir_id = c(0, 1))
+  expect_equal(unique(gtfs_filt_3$routes$route_id),
+                   expected = "804")
+  expect_equal(unique(gtfs_filt_3$trips$direction_id),
+                   expected = c(0, 1))
+  expect_s3_class(gtfs_filt_3,
+                  class = "tidygtfs")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+a
