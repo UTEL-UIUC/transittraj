d401a64f1a4678f769a57a4e32695249f06b196b
obrien-ben <obrienbenjaminj@gmail.com>
2026-07-29 12:25:49 -0500

    automated testing for plot_animated_line(), export_animation()

--- tests/testthat/test-trajectory_visualization.R
+++ tests/testthat/test-trajectory_visualization.R
@@ -60,6 +60,98 @@ test_that("plot_trajectory: plot layers", {
   )
 })
 
+# --- plot_animated_line() ---
+test_that("plot_animated_line: label validation", {
+
+  mono_df <- new_transittraj_data("make_monotonic")
+  traj <- get_trajectory_fun(mono_df)
+  feat_df <- data.frame(name = c("a"),
+                        distance = c(10000))
+
+  # label input val
+  expect_error(
+    plot_animated_line(trajectory = traj,
+                    feature_distances = feat_df,
+                    label_field = "missing"),
+    class = "error_plottraj_labels"
+  )
+  expect_error(
+    plot_animated_line(trajectory = traj,
+                    feature_distances = feat_df,
+                    label_field = "name",
+                    label_pos = "upside down"),
+    class = "error_plottraj_labels"
+  )
+})
+test_that("plot_animated_line: plot layers", {
+
+  mono_df <- new_transittraj_data("make_monotonic")
+  traj <- get_trajectory_fun(mono_df)
+  feat_df <- data.frame(name = c("a"),
+                        distance = c(10000))
+
+  # OK label & features
+  p_1 <- plot_animated_line(trajectory = traj,
+                         feature_distances = feat_df,
+                         label_field = "name")
+  # class
+  expect_s3_class(
+    p_1,
+    class = "gganim"
+  )
+  # layers: route, feature line, feature label, vehicles
+  expect_equal(
+    length(p_1$layers),
+    expected = 4
+  )
+  # distance lims
+  expect_equal(
+    min(p_1$layers[[1]]$data$distance),
+    expected = min(mono_df$distance),
+    tolerance = 0.01
+  )
+  expect_equal(
+    max(p_1$layers[[1]]$data$distance),
+    expected = max(mono_df$distance),
+    tolerance = 0.01
+  )
+
+  # with distance lims
+  test_lims <- c(5000, 15000)
+  p_2 <- plot_animated_line(trajectory = traj,
+                            feature_distances = feat_df,
+                            label_field = "name",
+                            distance_lims = test_lims)
+  # distance lims
+  expect_equal(
+    min(p_2$layers[[1]]$data$distance),
+    expected = min(test_lims),
+    tolerance = 0.01
+  )
+  expect_equal(
+    max(p_2$layers[[1]]$data$distance),
+    expected = max(test_lims),
+    tolerance = 0.01
+  )
+
+  # no label or features
+  p_3 <- plot_animated_line(trajectory = traj)
+  # layers: route, vehicles
+  expect_equal(
+    length(p_3$layers),
+    expected = 2
+  )
+})
+
+# --- export_animation() ---
+test_that("export_animation: name validation", {
+
+  expect_error(
+    export_animation(anim_object = "a",
+                     path = "incorrect ending"),
+    class = "error_trajplot_inputdata"
+  )
+})
 
 
 
